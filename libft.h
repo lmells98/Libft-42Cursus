ce7382350a3ff7f58d81a4bb3d19510a28a0d2d8
lmells98 <leightn.mells23@gmail.com>
2021-09-06 11:32:24 +0930

    Libft | libft.h

--- libft.h
+++ libft.h
@@ -8,7 +8,7 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
-int		ft_strlen(char *str);
+size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 
 #endif
