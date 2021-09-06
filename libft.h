5307e0cc6196115afacc2039c2d1aeb4a8c744d9
lmells98 <leightn.mells23@gmail.com>
2021-09-06 10:54:03 +0930

    Libft | libft.h

--- libft.h
+++ libft.h
@@ -1,12 +1,14 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_strlen(char *str);
-int	ft_strlcpy(char *dest, char *src, int n);
+# include <stdlib.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(char *str);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 
 #endif
