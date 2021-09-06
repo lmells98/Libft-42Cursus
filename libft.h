22c77d9df5a1fde1d0ba58d9ace4995fb164e58b
lmells98 <leightn.mells23@gmail.com>
2021-09-06 14:57:53 +0930

    Libft | libft.h

--- libft.h
+++ libft.h
@@ -10,6 +10,7 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+char	*ft_strdup(const char *s1);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
