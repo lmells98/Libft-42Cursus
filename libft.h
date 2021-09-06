413b67c5374233090ebfc63748d14d60c9ba77dd
lmells98 <leightn.mells23@gmail.com>
2021-09-06 14:24:40 +0930

    Libft | libft.h

--- libft.h
+++ libft.h
@@ -13,6 +13,7 @@ void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t len);
 size_t	ft_strlen(const char *str);
+size_t	ft_strlcat(char *dest, const char *src, size_t dest_size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 
 #endif
