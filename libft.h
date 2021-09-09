5776b92d9eb5b8e4c398458f74baabafe4ea4e0b
lmells98 <leighton.mells23@gmail.com>
2021-09-09 17:39:35 +0930

    libft.h | added ft_lstsize prototype

--- libft.h
+++ libft.h
@@ -26,6 +26,7 @@ int		ft_tolower(int c);
 int		ft_atoi(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_lstsize(t_list *lst);
 char	*ft_itoa(int n);
 char	*ft_strdup(const char *s1);
 char	*ft_strchr(const char *s, int c);
