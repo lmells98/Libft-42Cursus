666bb9887358323db2f03177767688b935d57769
lmells98 <leighton.mells23@gmail.com>
2021-09-09 17:59:02 +0930

    libft.h | added ft_lstdelone prototype

--- libft.h
+++ libft.h
@@ -45,6 +45,7 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void(*del)(void *));
 void	*ft_calloc(size_t count, size_t size);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
